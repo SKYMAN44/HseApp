e37a4855001fa4ed22aeef436d7590f1d95219ee
Dmitrii Sokolov <sokdep@yandex.ru>
2022-02-23 02:47:45 +0300

    remove Fonts

--- HSE/Main/AppDelegate.swift
+++ HSE/Main/AppDelegate.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import HSESKIT
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        HSESKIT.registerFonts()
         return true
     }
 
