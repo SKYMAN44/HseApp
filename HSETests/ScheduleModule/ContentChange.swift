50aaffe0570dde356e77a3044f2e761e8bb4eed1
skyman <sokdep@yandex.ru>
2022-06-10 15:12:13 +0300

    remove legacy

--- HSETests/ScheduleModule/ContentChange.swift
+++ HSETests/ScheduleModule/ContentChange.swift
@@ -8,32 +8,5 @@
 import XCTest
 @testable import HSE
 
-class ContentChange: XCTestCase {
-    var schViewModel: ScheduleViewModel!
-    var tableVIew = UITableView()
+// lega
 
-    override func setUpWithError() throws {
-        try super.setUpWithError()
-        schViewModel = ScheduleViewModel(tableView: tableVIew, NetworkManager())
-    }
-
-    override func tearDownWithError() throws {
-        schViewModel = nil
-        try super.tearDownWithError()
-    }
-
-    func testSortingDeadlines() throws {
-        let closure = {
-            let deadlinesCW = self.schViewModel.currentdeadlines
-            deadlinesCW.forEach {
-                $0.assignments.forEach {
-                    XCTAssertEqual($0.type, .cw, "Assignment type not matching cw")
-                }
-            }
-        }
-        schViewModel.contentChanged(contentType: .assigments)
-        schViewModel.bindScheduleViewModelToController = closure
-        schViewModel.deadLineContentChanged(.hw)
-        
-    }
-}
